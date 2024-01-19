c30df256c9b40f2dd41084fc521f4a3073ac8e9b
Nicole <nicolefongjw@gmail.com>
2024-01-19 13:19:53 -0800

    Initial Commit

---
+++ RechargeTeach/RechargeTeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  RechargeTeachApp.swift
+//  RechargeTeach
+//
+//  Created by Nicole Fong on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RechargeTeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RechargeTeachUITests/RechargeTeachUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RechargeTeachUITestsLaunchTests.swift
+//  RechargeTeachUITests
+//
+//  Created by Nicole Fong on 1/19/24.
+//
+
+import XCTest
+
+final class RechargeTeachUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
